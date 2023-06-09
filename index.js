88d2ec5e352f5e466d848937fdd7b609279aaee3
lydia tschoe <lydiatschoe@gmail.com>
2023-06-08 21:36:45 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+// index.js
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 4}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
+  
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
